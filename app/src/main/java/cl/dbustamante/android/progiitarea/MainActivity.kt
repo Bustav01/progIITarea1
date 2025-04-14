2030ac4a6ea0a19dd45d982a04ce9fde9745a00b
Bustav01 <diegobenelohim@gmail.com>
2025-04-14 02:46:57 -0400

    inicialización de lógica en Activity

--- app/src/main/java/cl/dbustamante/android/progiitarea/MainActivity.kt
+++ app/src/main/java/cl/dbustamante/android/progiitarea/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cl.dbustamante.android.progiitarea.controllers.CuentaController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  cuentaController: CuentaController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        cuentaController = CuentaController()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
